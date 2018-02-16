c6be2643fe85ab812d85091228958be154709b57
prakashsatya168 <prakashsatya168@gmail.com>
2018-02-15 23:58:18 -0500

    message

---
+++ app/Http/routes.php
@@ -0,0 +1,8 @@
+<?php
+
+
+Route::get('/homepage', function () {
+    return view('homepage');
+});
+
+
---
+++ resources/views/register.blade.php
@@ -0,0 +1,7 @@
+<html>
+  <body>
+    <h1>
+     REGISTER HERE
+    </h1>
+  </body>
+</html>
